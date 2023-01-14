363fdaf4bd2c72ccc5cba86bb96fb4371aca711a
kolegran <kolegran@gmail.com>
2023-01-14 05:44:07 +0200

    Refactored MappingProcessor: main method has been splitted into logical points, each of which has been extracted in a separate method

--- src/main/java/annotations/processors/MappingProcessor.java
+++ src/main/java/annotations/processors/MappingProcessor.java
@@ -8,10 +8,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class MappingProcessor {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(MappingProcessor.class);
+    private static final String SPACE = " ";
+    private static final String COMA = ",";
+    private static final String SEMICOLON = ";";
+    private static final String LEFT_BRACKET = "(";
+    private static final String RIGHT_BRACKET = ")";
+    private static final String CREATE_TABLE = "CREATE TABLE";
+    private static final String PRIMARY_KEY_CONSTRAINT = "PRIMARY KEY";
+    private static final int SINGLE_ID_MARKER = 1;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MappingProcessor.class);
 
     private final QueryExecutor queryExecutor;
 
@@ -20,35 +33,86 @@ public class MappingProcessor {
     }
 
     public void createTable(Class<?> entityClass) {
+        final StringBuilder query = buildInitialQueryPart(entityClass);
+
+        final Field[] fields = entityClass.getDeclaredFields();
+
+        query.append(processEntityId(fields, query));
+        query.append(processColumns(fields, query));
+        query.append(buildFinalQueryPart(query));
+
+        queryExecutor.execute(query.toString());
+    }
+
+    // looks too useless, but I want to emphasize the idea
+    private StringBuilder buildFinalQueryPart(StringBuilder query) {
+        return query.append(RIGHT_BRACKET).append(SEMICOLON);
+    }
+
+    private StringBuilder buildInitialQueryPart(Class<?> entityClass) {
         EntityMapping entityMapping = entityClass.getAnnotation(EntityMapping.class);
-        if (entityMapping == null) {
+        if (isNull(entityMapping)) {
             LOGGER.error("EntityMapping annotation was not found");
             throw new EntityMappingNotFoundException("Entity class must be annotated with @EntityMapping");
         }
-        Field[] fields = entityClass.getDeclaredFields();
-        StringBuilder queryString = new StringBuilder("CREATE TABLE " + entityMapping.name() + " (");
-        String primaryKeyName = "";
-        for (Field field : fields) {
-            IdMapping idMapping = field.getAnnotation(IdMapping.class);
-            if (idMapping != null) {
-                primaryKeyName = field.getName();
-                queryString.append(primaryKeyName + " " + idMapping.type() + ", ");
-            } else {
-                ColumnMapping columnMapping = field.getAnnotation(ColumnMapping.class);
-                if (columnMapping == null) {
-                    continue;
-                }
-                queryString.append(field.getName() + " " + DataTypeMapper.getType(field.getType()) + ", ");
-            }
+
+        return new StringBuilder()
+            .append(CREATE_TABLE).append(SPACE)
+            .append(entityMapping.name()).append(SPACE)
+            .append(LEFT_BRACKET);
+    }
+
+    private StringBuilder processEntityId(Field[] fields, StringBuilder query) {
+        final Field entityId = findSingleId(fields);
+        IdMapping idMapping = entityId.getAnnotation(IdMapping.class);
+        String idName = idMapping.name().isEmpty()
+            ? entityId.getName()
+            : idMapping.name();
+
+        return query
+            .append(idName).append(SPACE)
+            .append(idMapping.type()).append(SPACE)
+            .append(PRIMARY_KEY_CONSTRAINT).append(COMA).append(SPACE);
+    }
+
+    private Field findSingleId(Field[] declaredFields) {
+        List<Field> ids = Arrays.stream(declaredFields)
+            .filter(field -> nonNull(field.getAnnotation(IdMapping.class)))
+            .toList();
+
+        if (ids.size() == SINGLE_ID_MARKER) {
+            return ids.get(0);
+        } else {
+            throw new MoreThanOneIdException("Entity class should have one single Id");
         }
-        queryString.append("PRIMARY KEY (" + primaryKeyName + ")");
-        queryString.append(")");
-        queryExecutor.execute(queryString.toString());
+    }
+
+    private StringBuilder processColumns(Field[] fields, StringBuilder query) {
+        for (Field field : fields) {
+            ColumnMapping columnMapping = field.getAnnotation(ColumnMapping.class);
+
+            if (isNull(columnMapping)) {
+                query.append(field.getName()).append(SPACE)
+                    .append(DataTypeMapper.getType(field.getType()));
+            } else {
+                query.append(columnMapping.name()).append(SPACE)
+                    .append(DataTypeMapper.getType(columnMapping.type().getClass()));
+            }
+            query.append(COMA).append(SPACE);
+        }
+        return query;
     }
 
     private static final class EntityMappingNotFoundException extends RuntimeException {
 
-        public EntityMappingNotFoundException(String message) {
+        EntityMappingNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    private static final class MoreThanOneIdException extends RuntimeException {
+
+        public MoreThanOneIdException(String message) {
             super(message);
         }
     }
